16b3fee15bec1a12dea7ab7d3d11e6bf71bbfe80
Gaurav Hegde <gauravhegde03@gmail.com>
2023-10-16 14:22:14 +0530

    feat: Added routes to the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import Player from "./Pages/Player";
+import TvShow from "./Pages/TvShow";
+import Netflix from "./Pages/Netflix";
+import MoviePage from "./Pages/MoviePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/signup" element={<SignUpPage />} />
+        <Route exact path="/player" element={<Player />} />
+        <Route exact path="/tv" element={<TvShow />} />
+        <Route exact path="/" element={<Netflix />} />
+        <Route exact path="/movie" element={<MoviePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
