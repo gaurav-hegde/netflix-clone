a2b5c2277b0013368a5a60ce928888440040b1d9
Gaurav Hegde <gauravhegde03@gmail.com>
2023-10-17 12:35:19 +0530

    feat: Navigation bar added with signout functionality, fix: index.css changes made for global styling

---
+++ src/Components/TopNav.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { AiOutlineLogout } from "react-icons/ai";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import { firebaseAuth } from "../utils/firebase-config";
+
+import { signOut, onAuthStateChanged } from "firebase/auth";
+
+const TopNav = ({ isScrolled }) => {
+  const navlinks = [
+    { name: "Home", link: "/" },
+    { name: "TV Show", link: "/tv" },
+    { name: "My List", link: "/mylist" },
+    { name: "Movies", link: "/movies" },
+  ];
+
+  const navigate = useNavigate();
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate("/login");
+  });
+
+  return (
+    <NavContainer>
+      <nav className={`${isScrolled ? "scrolled" : "notScrolled"}`}>
+        <div className="leftSide">
+          <div className="logo">
+            <img
+              src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668265433/NetflixApp/2560px-Netflix_2015_logo.svg_rbicwl_knwp6f.png"
+              alt="logo"
+            />
+          </div>
+          <ul className="links">
+            {navlinks.map(({ name, link }) => {
+              return (
+                <li key={name}>
+                  <Link to={link}>{name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="rightSide">
+          <button onClick={() => signOut(firebaseAuth)}>
+            <AiOutlineLogout />
+          </button>
+        </div>
+      </nav>
+    </NavContainer>
+  );
+};
+
+const NavContainer = styled.div`
+  .notScrolled {
+    display: flex;
+  }
+  .scrolled {
+    display: flex;
+    background-color: black;
+  }
+  nav {
+    position: sticky;
+    top: 0;
+    height: 6rem;
+    width: 100%;
+    justify-content: space-between;
+    position: fixed;
+    z-index: 2;
+    padding: 0.4rem;
+    align-items: center;
+    transition: 0.3s ease-in-out;
+    .leftSide {
+      display: flex;
+      align-items: center;
+      gap: 2rem;
+    }
+    .logo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    img {
+      width: 10rem;
+      height: 3rem;
+      margin: 1rem;
+    }
+    .links {
+      display: flex;
+      list-style-type: none;
+      gap: 2rem;
+      li {
+        a {
+          text-decoration: none;
+          color: white;
+        }
+      }
+    }
+    .rightSide {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      button {
+        background-color: red;
+        border: none;
+        cursor: pointer;
+        border-radius: 50%;
+        padding: 0.1rem;
+        margin: 1rem;
+      }
+      &:focus {
+        outline: none;
+      }
+      svg {
+        color: white;
+        font-size: 2rem;
+      }
+    }
+  }
+`;
+
+export default TopNav;
--- src/Pages/Netflix.js
+++ src/Pages/Netflix.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import TopNav from "../Components/TopNav";
 
 const Netflix = () => {
-  return <div>Netflix</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.scrollY === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  console.log(isScrolled);
+
+  return (
+    <div className="hero">
+      <TopNav isScrolled={isScrolled} />
+      <img
+        src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+        alt="hero"
+      />
+      <img
+        src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+        alt="hero"
+      />
+      <img
+        src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+        alt="hero"
+      />
+      <img
+        src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+        alt="hero"
+      />
+      <img
+        src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg"
+        alt="hero"
+      />
+    </div>
+  );
 };
 
 export default Netflix;
